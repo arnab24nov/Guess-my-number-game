e1d3c607a80974e187e2c3ab2976a14064a5640f
arnab24nov <87443969+arnab24nov@users.noreply.github.com>
2021-08-13 17:44:36 +0530

    This is script file

--- Guess_my_number.js
+++ Guess_my_number.js
@@ -1,38 +1,40 @@
 'use strict'
 let numRandom = Math.trunc(Math.random()*20) +1;
 
+function showMessege(msg){
+    document.querySelector(".messege").textContent = msg;
+}
+
 document.querySelector(".btn2").addEventListener('click', function(){
     let gNum = Number(document.querySelector(".guessNumber").value);
     if (gNum < 1 || gNum > 20){
-        document.querySelector(".messege").innerHTML = "Please put a number between 1 to 20.";
+        showMessege("Put a number between 1 to 20.");
     }else{
 
     let sCore =Number(document.querySelector(".score").textContent);
 
     if (sCore >0){
     if (sCore==1){
-        document.querySelector(".messege").innerHTML = "Ops... You lost the game!";
+        showMessege("Ops... You lost the game!");
         document.querySelector(".score").textContent = 0;
         document.body.classList.add("myStyle2");
         document.querySelector(".number").textContent = numRandom;
     }else{
     if (gNum < numRandom){
-        document.querySelector(".messege").innerHTML = "Too low!";
+        showMessege("Too low!");
         let sCore =Number(document.querySelector(".score").textContent);
         sCore--;
         document.querySelector(".score").textContent = " " + sCore;
     }
     if (gNum > numRandom){
-        document.querySelector(".messege").innerHTML = 
-        "Too high!"; 
+        showMessege("Too high!"); 
         let sCore =Number(document.querySelector(".score").textContent);
         sCore--;
         document.querySelector(".score").textContent = " " + sCore;
     }
     if (gNum == numRandom){
         document.querySelector(".number").textContent = numRandom;
-        document.querySelector(".messege").innerHTML = 
-        "YAY...You win the game."; 
+        showMessege("YAY...You win the game."); 
         let highSore =Number(document.querySelector(".high-score").textContent) ;
         document.body.classList.add("myStyle1");
         if(sCore > highSore){
@@ -50,6 +52,6 @@ document.querySelector(".btn1").addEventListener('click', function(){
     document.body.classList.remove("myStyle1");
     document.body.classList.remove("myStyle2");
     numRandom = Math.trunc(Math.random()*20) +1;
-    document.querySelector(".messege").innerHTML = "Start Guessing....";
+    showMessege("Start Guessing....");
 
 })
